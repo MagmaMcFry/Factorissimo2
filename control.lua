f1d988ba263dabe510daebfdaefd9b4712476373
MagmaMcFry <magmamcfry@gmail.com>
2020-11-27 19:26:45 +0100

    Overlay rework: Overlay generation, layout algorithm

--- control.lua
+++ control.lua
@@ -305,16 +305,6 @@ end
 
 -- FACTORY UPGRADES --
 
---[[
-local function build_power_upgrade(factory)
-	if factory.upgrades.power then return end
-	factory.upgrades.power = true
-	local iet = factory.inside_surface.create_entity{name = "factory-power-pole", position = {factory.inside_x + factory.layout.inside_energy_x, factory.inside_y + factory.layout.inside_energy_y}, force = factory.force}
-	iet.destructible = false
-	table.insert(factory.inside_other_entities, iet)
-end
-]]--
-
 local function build_lights_upgrade(factory)
 	if factory.upgrades.lights then return end
 	factory.upgrades.lights = true
@@ -340,6 +330,7 @@ function build_display_upgrade(factory)
 		},
 		force = factory.force
 	}
+	controller.minable = false
 	controller.destructible = false
 	controller.rotatable = false
 	factory.inside_overlay_controller = controller
@@ -347,7 +338,8 @@ end
 
 -- OVERLAY MANAGEMENT --
 
-local function draw_overlay_sprite(sprite_name, target_entity, offset, scale, id_table)
+local function draw_overlay_sprite(signal, target_entity, offset, scale, id_table)
+	local sprite_name = signal.type .. "/" .. signal.name
 	if target_entity.valid then
 		local sprite_data = {
 			sprite = sprite_name,
@@ -356,17 +348,55 @@ local function draw_overlay_sprite(sprite_name, target_entity, offset, scale, id
 			target = target_entity,
 			surface = target_entity.surface,
 			only_in_alt_mode = true,
+			render_layer = "entity-info-icon",
 		}
-		local shadow_radius = 0.2
+		-- Fake shadows
+		local shadow_radius = 0.07 * scale
 		for _, shadow_offset in pairs({{0,shadow_radius}, {0, -shadow_radius}, {shadow_radius, 0}, {-shadow_radius, 0}}) do
 			sprite_data.tint = {0, 0, 0, 0.5} -- Transparent black
 			sprite_data.target_offset = {offset[1] + shadow_offset[1], offset[2] + shadow_offset[2]}
 			table.insert(id_table, rendering.draw_sprite(sprite_data))
 		end
+		-- Proper sprite
 		sprite_data.tint = nil
 		sprite_data.target_offset = offset
 		table.insert(id_table, rendering.draw_sprite(sprite_data))
 	end
+end
+
+local function get_nice_overlay_arrangement(width, height, amount)
+	-- Computes a nice arrangement of square sprites within a rectangle of given size
+	-- Returned coordinates are relative to the center of the rectangle
+	if amount <= 0 then return {} end
+	local opt_rows = 1
+	local opt_cols = 1
+	local opt_scale = 0
+	-- Determine the optimal number of rows to use
+	-- This assumes width >= height
+	for rows = 1, math.ceil(math.sqrt(amount)) do
+		local cols = math.ceil(amount/rows)
+		local scale = math.min(width/cols, height/rows)
+		if scale > opt_scale then
+			opt_rows = rows
+			opt_cols = cols
+			opt_scale = scale
+		end
+	end
+	-- Adjust scale to ensure that sprites do not become too big
+	opt_scale = math.pow(opt_scale, 0.8) * math.pow(1.5, 0.8 - 1)
+	-- Create evenly spaced coordinates
+	local result = {}
+	for i = 0, amount-1 do
+		local col = i % opt_cols
+		local row = math.floor(i / opt_cols)
+		local cols_in_row = (row < opt_rows - 1 and opt_cols or (amount - 1) % opt_cols + 1)
+		table.insert(result, {
+			x = (2 * col + 1 - cols_in_row) * width / (2 * opt_cols),
+			y = (2 * row + 1 - opt_rows) * height / (2 * opt_rows),
+			scale = opt_scale
+		})
+	end
+	return result
 end
 
 function update_overlay(factory)
@@ -374,10 +404,30 @@ function update_overlay(factory)
 		rendering.destroy(id)
 	end
 	factory.outside_overlay_displays = {}
-	if factory.built then -- TODO remove test code
-		draw_overlay_sprite("item/iron-plate", factory.building,
-			{0, 0}, 2,
-		factory.outside_overlay_displays)
+	if factory.built and factory.inside_overlay_controller and factory.inside_overlay_controller.valid then
+		local params = factory.inside_overlay_controller.get_or_create_control_behavior().parameters
+		local nonempty_params = {}
+		for _, param in pairs(params) do
+			if param and param.signal and param.signal.name then
+				table.insert(nonempty_params, param)
+			end
+		end
+		local sprite_positions = get_nice_overlay_arrangement(
+			factory.layout.overlays.outside_w,
+			factory.layout.overlays.outside_h,
+			#nonempty_params
+		)
+		local i = 0
+		for _, param in pairs(nonempty_params) do
+			i = i + 1
+			draw_overlay_sprite(param.signal, factory.building,
+				{
+					sprite_positions[i].x + factory.layout.overlays.outside_x,
+					sprite_positions[i].y + factory.layout.overlays.outside_y,
+				},
+				sprite_positions[i].scale,
+			factory.outside_overlay_displays)
+		end
 	end
 end
 
